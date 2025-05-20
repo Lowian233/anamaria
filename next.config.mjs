ecdb35c51e4bdbbe8146e156cae36bc599802236
Lowian Pinzon <lpinzon@notxus.com>
2025-05-19 20:12:00 -0500

    primer vista de la pagina

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
 
 const nextConfig = {
   images: {
